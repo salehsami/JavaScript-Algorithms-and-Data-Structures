1a88b7453c29488984937bf1851c81b48832b2fe
Saleh Sami <67518300+salehsami@users.noreply.github.com>
2022-11-24 22:58:46 +0500

    Add files via upload

---
+++ Basic JavaScript/commnets.js
@@ -0,0 +1,18 @@
+/*
+Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+
+There are two ways to write comments in JavaScript:
+
+Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
+
+// This is an in-line comment.
+You can make a multi-line comment beginning with /* and ending with */
+
+/* This is a
+multi-line comment */
+
+
+//single line comment
+/* multi
+line 
+comment*/
